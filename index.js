507f8f79025fe11e8c8371883ff3eae3cf8e8dbe
Felix-Mj <felix.mj@yahoo.com>
2022-12-23 02:17:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const contact = document.querySelector('.fa-sharp')
+const contacts = document.querySelector('ul')
+
+contact.addEventListener('click', ()=>{
+    contacts.classList.toggle('showContact')
+    console.log("hello")
+})
